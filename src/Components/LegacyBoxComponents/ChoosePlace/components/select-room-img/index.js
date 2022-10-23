b35a0bde97aaed01d2d783f987355d4003192f88
EgorkaPl <egorplatonov2002@gmail.com>
2022-10-23 09:09:25 +0300

    fix-in-images

--- src/Components/LegacyBoxComponents/ChoosePlace/components/select-room-img/index.js
+++ src/Components/LegacyBoxComponents/ChoosePlace/components/select-room-img/index.js
@@ -1,9 +1,10 @@
 import './index.scss';
+import  IMAGE  from '../../../../../Icons/test.png';
 
 export const SelectRoomImg = () => {
     return (
         <div className='selectRoomImg'>
-            
+            <img src={IMAGE} width="100%" height="100%" />
         </div>
     )
 }
