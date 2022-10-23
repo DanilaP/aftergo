3e5d0535bd6e2521a02cadfaf83cffae68c4b0cd
EgorkaPl <egorplatonov2002@gmail.com>
2022-10-23 16:27:38 +0300

    fix-in-stepper

--- src/Components/LegacyBoxComponents/MyPersonalData/components/CheckSettings/index.js
+++ src/Components/LegacyBoxComponents/MyPersonalData/components/CheckSettings/index.js
@@ -1,11 +1,37 @@
+import { useState } from 'react';
+import Checkbox from '../../../../../Icons/checkbox.png';
+
 import './index.scss';
 
 export const CheckSettings = () => {
+
+    const [policy, setPolicy] = useState(false);
+    const [informed, setInformed] = useState(false);
+
+
+    const onChecked = (e) => {
+        if (e.target.name === 'policy') {
+            setPolicy(!policy);
+            return;
+        };
+        if (e.target.name === 'informed') {
+            setInformed(!informed);
+            return;
+        }
+    }
     return (
         <div className='checkSettings'>
             <div className='checkSettings__checkboxes'>
-                <input type="checkbox" />
-                <input type="checkbox" />
+                <div className='checkbox__block'>
+                    <button onClick={(e) => onChecked(e)} className='checkbox' name='policy'>
+                        {policy && <img src={Checkbox} width="20px" height="20px" name='policy'/>}
+                    </button>
+                </div>
+                <div className='checkbox__block'>
+                    <button onClick={(e) => onChecked(e)} className='checkbox' name='informed'>
+                        {informed && <img src={Checkbox} width="20px" height="20px" name='informed'/>}
+                    </button>
+                </div>
             </div>
             <div className='checkSettings__values'>
                 <span>I agree to Privacy Legacy & Terms and Conditions  </span>
