73eedc00fd8ce0dc46fb07f7145ba5f2a32f3a26
EgorkaPl <egorplatonov2002@gmail.com>
2022-10-23 19:19:52 +0300

    fix-in-styles-order-forms

--- src/Components/LegacyBoxComponents/ChoosePlace/components/info-block/index.js
+++ src/Components/LegacyBoxComponents/ChoosePlace/components/info-block/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 export const InfoBlock = ({ text }) => {
     return (
         <div className='infoBlock'>
-            <span className='infoBlock__mainText'>Subscribtion</span>
+            <span className='infoBlock__mainText'>Subscription</span>
             <span className='infoBlock__subText'>{text}</span>
         </div>
     )
